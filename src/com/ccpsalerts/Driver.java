494c02586a688e3db999f24c548cc941df9d9bc9
KRatti <kyle.ratti@outlook.com>
2014-02-27 22:25:23 -0500

    Added logging

--- src/com/ccpsalerts/Driver.java
+++ src/com/ccpsalerts/Driver.java
@@ -5,11 +5,29 @@
 
 package com.ccpsalerts;
 
+import java.util.Date;
+
+import java.text.SimpleDateFormat;
+
 public class Driver
 {
+	private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("h:m:s a");
+
 	public static void main(String[] args)
 	{
 		CronService objCron = new CronService();
 		objCron.start();
+
+		Driver.println("CronService started");
+	}
+
+	public static void println(String strMsg)
+	{
+		System.out.printf("[%s] %s%n", Driver.simpleDateFormat.format(new Date()), strMsg);
+	}
+
+	public static void errorln(String strMsg)
+	{
+		Driver.println("ERROR: " + strMsg);
 	}
 }
