0643e8049501e444cb4002add690243ff8a1e789
KRatti <kyle.ratti@outlook.com>
2014-04-16 18:56:48 -0400

    Debugging

--- src/compile_run.bat
+++ src/compile_run.bat
@@ -4,7 +4,7 @@ goto compile_run
 
 :compile_run
 javac -cp ".;..\lib\*" com\ccpsalerts\*.java
-java -cp ".;..\lib\*" com.ccpsalerts.Driver -task websitechecker
+java -cp ".;..\lib\*" com.ccpsalerts.Driver -task lastdaycountdown
 goto delete_files
 
 :delete_files
