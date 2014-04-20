064e25ca091a04d07253a21464be726a5f0b4c7e
KRatti <kyle.ratti@outlook.com>
2014-04-20 10:35:22 -0400

    Updated compile_run.sh

--- src/compile_run.sh
+++ src/compile_run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-javac -cp ".:../lib/*" com/ccpsalerts/*.java
+rm com/ccpsalerts/*.class com/ccpsalerts/calendar/*.class
+javac -cp ".:../lib/*" com/ccpsalerts/*.java com/ccpsalerts/calendar/*.java
 java -cp ".:../lib/*" com.ccpsalerts.Driver
